6db93d08ca3b14a36e6828090c221d917ae46d2e
Alba Panato Alegre <alba.panato.alegre.apa@gmail.com>
2024-09-30 03:26:42 +0200

    AE203 (CE1.f) Editores de código vs IDEs

---
+++ entorno_de_desarrollo/unidad2/proyecto_.NET/suma/Program.cs
@@ -0,0 +1,19 @@
+﻿class suma
+{
+    static void Main()
+    {
+        //Solicitando 1ªnumero
+        Console.Write("Ingrese el primer número entero --> ");
+        int numero1 = Convert.ToInt32(Console.ReadLine());
+
+        //Solicitando 2ªnumero
+        Console.Write("Ingrese el segundo número entero --> ");
+        int numero2 = Convert.ToInt32(Console.ReadLine());
+
+        //Calcular la suma
+        int suma = numero1 + numero2;
+
+        //Mostrar el resultado
+        Console.Write("El resultado de la suma es = " + suma + " ");
+    }
+}
